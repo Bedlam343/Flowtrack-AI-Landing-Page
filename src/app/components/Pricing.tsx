b38d40315071964f5815c017ed09e7691659e791
bedlam343 <jagik22@gmail.com>
2025-11-28 17:33:48 -0800

    trigger appear animation only once

--- src/app/components/Pricing.tsx
+++ src/app/components/Pricing.tsx
@@ -5,6 +5,7 @@ const appearAnimationProps = {
   initial: { opacity: 0, y: 20 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.5 },
+  viewport: { once: true },
 };
 
 const Pricing = () => {
--- src/app/components/TechDeepDive.tsx
+++ src/app/components/TechDeepDive.tsx
@@ -6,6 +6,7 @@ const appearAnimationProps = {
   initial: { opacity: 0, y: 20 },
   whileInView: { opacity: 1, y: 0 },
   transition: { duration: 0.5 },
+  viewport: { once: true },
 };
 
 const TechDeepDive = () => {
